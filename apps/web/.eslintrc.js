11be938f61f391ea2b806bc675229b621ab7fe0a
Dennis <denniarems@gmail.com>
2022-12-03 21:25:42 +0530

    :bug:

--- apps/web/.eslintrc.js
+++ apps/web/.eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-    root: true,
-    env: {
-        node: true,
-    },
-    extends: [
-        'plugin:vue/vue3-essential',
-        'eslint:recommended',
-        '@vue/typescript/recommended',
-        'plugin:prettier/recommended',
-    ],
-    parserOptions: {
-        ecmaVersion: 2020,
-    },
-    rules: {
-        // semi: ["error", "never"],
-        // quotes: ["error", "single"],
-        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    },
-}
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    // semi: ["error", "never"],
+    // quotes: ["error", "single"],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
