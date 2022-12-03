640ba6ad7be3f49466f0bef2c9a17e719744f487
mekhakrishnanm <mekhakrishnanm@gmail.com>
2022-12-03 21:21:01 +0530

    :ambulance:

--- apps/web/src/router/index.ts
+++ apps/web/src/router/index.ts
@@ -1,20 +1,20 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
+const childRoutes = (prop: string, mode = false) => [
+  {
+    path: '',
+    name: prop + '.list',
+    meta: { auth: true, name: 'Social App' },
+    component: () => import('../views/Home/Home.vue')
+  }
+]
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: '/',
+    name: 'home',
+    component: () => import('../layouts/homeLayout.vue'),
+    children: childRoutes('home')
   },
 ];
 
