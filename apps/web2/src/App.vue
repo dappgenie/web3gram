c25ff5e82a2bbbd22ee4f1f5451d9eab8bc62e16
Dennis <denniarems@gmail.com>
2022-12-03 20:04:13 +0530

    :pencil:

--- apps/web2/src/App.vue
+++ apps/web2/src/App.vue
@@ -1,3 +1,13 @@
+<script lang="ts" setup>
+import { Web3Storage, getFilesFromPath } from "web3.storage";
+import SocialLogin from "@biconomy/web3-auth";
+import SmartAccount from "@biconomy/smart-account";
+
+const storage = new Web3Storage({
+  token: `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDQyRDExNzBhMzk2OWUwOEQyRDBEM2FlZkI4OTdFMTIzNzgyMkE4YTMiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NzAwMzEyMTYyODEsIm5hbWUiOiJ3ZWIzZ3JhbSJ9.9Ugzn3pR_F87EmvKpVB2EsCbdeFSJ-ccuT4EcGh9Pas`,
+});
+const socialLoginSDK = new SocialLogin();
+</script>
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
