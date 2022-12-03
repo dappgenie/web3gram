92b1ddb819f8901a06a652ff894e261796a9ea19
Anas K V M <anas@terrain.art>
2022-12-03 05:41:47 +0530

    EIP-2771

---
+++ apps/fvm-deployer/hardhat.config.js
@@ -0,0 +1,22 @@
+require('hardhat-deploy');
+require('hardhat-deploy-ethers');
+require("dotenv").config()
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  defaultNetwork: "wallaby",
+  networks: {
+    wallaby: {
+      url: "https://wallaby.node.glif.io/rpc/v0",
+      accounts: [PRIVATE_KEY],
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+};
